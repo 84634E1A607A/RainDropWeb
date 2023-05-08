1b00685b67ddbf7ece7e6891509b8eb34347f39f
Ajax <ajax-dong@outlook.com>
2023-05-08 21:00:38 +0800

    Razor pages wrapper and Index

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 app.UseRouting();
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
-app.MapFallbackToFile("/", "index.html");
-app.MapFallbackToFile("/Index", "index.html");
+app.MapRazorPages();
 app.MapFallbackToFile("/Oscilloscope", "oscilloscope.html");
 app.MapFallbackToFile("/Supply", "supply.html");
 app.MapFallbackToFile("/WaveGenerator", "wave-generator.html");
